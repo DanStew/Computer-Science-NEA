71f4130fec8f859a3e72225cf645d5647aa3fce7
DanStew <90531180+DanStew@users.noreply.github.com>
2022-07-16 03:49:47 +0100

    Table setup

--- JavascriptFiles/script3.js
+++ JavascriptFiles/script3.js
@@ -1,6 +1,7 @@
 function checkWebsite(){
-    let tournamentType = JSON.parse(localStorage.getItem("tournamentType"))
-    if (tournamentType == "Knockout"){
+    let tournamentType = localStorage.getItem("tournamentType")
+    console.log(tournamentType)
+    if (tournamentType == "knockout"){
         window.location.href = "Knockout.html"
     }
     else{
@@ -8,3 +9,23 @@ function checkWebsite(){
     }
 }
 
+//Function to put back together the array of instances of the class with the saved data
+function remakeObjects(){
+    let teamNmb = JSON.parse(localStorage.getItem("teamNmb"))
+    let teamNmbs = new Array(teamNmb)
+    for (let i=1 ; i<=teamNmb ; i++){
+        let  pTeamNmb= localStorage.getItem("TeamNmb" + i + "Nmb")
+        let  pTeamNmbCounter= JSON.parse(localStorage.getItem("TeamNmb" + i + "NmbCounter"))
+        let  pTeamName= localStorage.getItem("TeamNmb" + i + "Name")
+        let  pStanding =  JSON.parse(localStorage.getItem("TeamNmb" + i + "Standing"))
+        let  pWins= JSON.parse(localStorage.getItem("TeamNmb" + i + "Wins"))
+        let  pDraws= JSON.parse(localStorage.getItem("TeamNmb" + i + "Draws"))
+        let  pLosses= JSON.parse(localStorage.getItem("TeamNmb" + i + "Losses"))
+        let  pPts= JSON.parse(localStorage.getItem("TeamNmb" + i + "Pts"))
+        let  pGf= JSON.parse(localStorage.getItem("TeamNmb" + i + "Gf"))
+        let  pGa= JSON.parse(localStorage.getItem("TeamNmb" + i + "Ga"))
+        let  pGd= JSON.parse(localStorage.getItem("TeamNmb" + i + "Gd"))
+        teamNmbs[i-1] = new teamArray(pTeamNmb, pTeamNmbCounter , pTeamName, pStanding, pWins, pDraws, pLosses, pPts, pGf, pGa, pGd)
+    }
+    return teamNmbs
+}
